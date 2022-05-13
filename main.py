5f8e320c171d5a6a481729e5a2834e3deadacc09
Kajetan-Grabara-Grabar <kajetan.grabara@gmail.com>
2022-05-13 19:05:06 +0200

    dodanie logów systemowy

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+import time #biblioteki programu
+with open ("micro.log","a+",encoding="UTF-8") as log:
+    t = time.localtime()
+    komunikat = time.strftime("%H:%M:%S", t)
+    komunikat+=" - Uruchomienie programu\n"
+    log.write(komunikat)
+
+
