7983d3bad3fc795ecd4f0cc3d7772ad9d99837fa
Kajetan-Grabara-Grabar <kajetan.grabara@gmail.com>
2022-05-13 19:32:50 +0200

    prace nad obsługą json

--- settings.py
+++ settings.py
@@ -3,9 +3,14 @@ import log
 def settings():
     try:
         with open("settings.json","r",encoding="UTF-8") as set:
-            print("test")
+            setting = json.dump(set)
+            print(setting)
     except:
         print("Nie można otworzyć pliku settings.json")
         log.log("Nie można otworzyć pliku settings.json")
+        setting = [True,True,False]
+        with open("settings.json","w+",encoding="UTF-8") as set:
+            json.dump(setting,set)
+
 if __name__ == '__main__':
     settings()
