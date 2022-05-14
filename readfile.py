2d907ef3f4e6fb9a45bef0ea1dcaab2e30eb04fd
Kajetan-Grabara-Grabar <kajetan.grabara@gmail.com>
2022-05-14 10:57:10 +0200

    plik readfile gotowy to działania

--- readfile.py
+++ readfile.py
@@ -1,10 +1,28 @@
 import log
+import re
 def readfile(name):
     try:
         with open(name,'r',encoding='UTF-8') as f:
             file = f.read()
+            #print(file)
+        words = re.findall("(.+);",file,flags=0)
+        definition = re.findall(";(.+)",file,flags=0)
+        print(len(words))
+        for i in range(len(words)):
+            print(words[i]+' -> '+definition[i])
+        if len(words) != len(definition):
+            print("Plik jest uszkodzony")
+            log.log("Plik "+name+' jest uszkodzony')
+            return 0, 0
+        else:
+            return words, definition
     except:
         print("Bląd otwarcia pliku "+name)
         log.log("Bląd otwarcia pliku "+name)
+        return 0, 0
 if __name__ == '__main__':
-    readfile("1.txt")
+    w, f = readfile("1.txt")
+    for i in range(len(w)):
+        print(w[i])
+    for i in range(len(f)):
+        print(f[i])
